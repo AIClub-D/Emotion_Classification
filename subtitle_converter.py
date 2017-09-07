db9ee246f9e7003992594515c8061c0f3d32aacc
hkang <kang2000h@naver.com>
2017-09-07 12:30:09 -0700

    add one hot encoding, Tagging, post processing

--- subtitle_converter.py
+++ subtitle_converter.py
@@ -3,18 +3,27 @@ import csv
 import numpy as np
 import konlpy
 import json
+import codecs
 
 def clean_text(text):
-    cond = "([a-z])|([&n])|<[^>]+>" # matching with eng song, <w*>, &w* part
-    p = re.compile(cond, re.I)
-    m = p.search(text)
-    if m:
+    cond1 = "(<BR>)|(</BR>)"  # matching with <BR>
+    cond2 = "([a-z])|([&nbsp;])|(<.+>)"  # matching with eng song, <w*>, &w* part
+    p1 = re.compile(cond1, re.I)
+    p2 = re.compile(cond2, re.I)
+    m1 = p1.search(text)
+    m2 = p2.search(text)
+    if m1 :
+        text = p1.sub("", text)
+        if m2:
+            text = p2.sub("", text)
+        return text
+    elif m2:
+        text = p2.sub("",text)
+        return text
+    elif len(text) == 0:
         text = ""
         return text
-    elif len(text)==0:
-        text=""
-        return text
-    else:
+    else: #normal
         return text
 
 #1. 질문 응답 셋을 만들어야한다. ? 나오면 질문, 그 다음 . or ! 나올때까지 받아놓고 응답으로 본다. (이후는 수작업가자.)
@@ -27,7 +36,7 @@ def extract_conversations(data):
     for line in data:
         #answer이 비었을때만 last_query 넣는다. ? 랑 !가 연속이면 저장해봄. 문제점1. !가 저장되버림 문제점2. 서로 물음표 쓰면서 대화하고 있을 때. 문제점3. 서로 마침표 쓰면서 대화할 때.
         #?를 포함하면 바로 선택, 바로 그 다음 응답만 선택
-        pattern = "([/?]+)"
+        pattern = "([?]+)"
         p = re.compile(pattern)
         m = p.search(line)
         if m and len(answer) == 0:
@@ -49,7 +58,8 @@ def extract_conversations(data):
     return (OQ, OA)
 
 #2. 같은 사람이 이어서 말한건데 자막이 줄을 바꿨으면?  모든 라인을 돌면서, 마침표('.')가 없이 마치면 잘린걸로 보고 다음 line을 그 앞 line에 붙인다.
-#근데 .로 마치거나 -로 마치면 정상적으로 넘어간걸로 본다. (문제점 : !로 이어지더라도 잘라버림.)
+#근데 .로 마치거나 -로 마치면 정상적으로 넘어간걸로 본다. (문제점 : !로 이어지
+# 라도 잘라버림.)
 def attach_sameturn(data):
     '''    
     :param data: list of data 
@@ -72,7 +82,7 @@ def attach_sameturn(data):
                 temp += line
     return result
 
-#3. 너무 긴 질문이나 응답은 세트로 날리기(마지막)
+#3. 너무 긴 질문이나 응답은 세트로 날리기
 def QA_cutter(query, answer):
     remove_query_list = []
     remove_answer_list = []
@@ -108,16 +118,15 @@ def noun_fetcher(datas, targets, substitutions):
                 break
     return datas
 
-def subtitle_converter(input_filename, output_filename):
-    f = open(input_filename, 'r')
-
+def subtitle_converter(input_filename, output_filename, encoding='utf-8'):
     #getting data preprocessed
     data = []
-    for line in f.read().splitlines():
+    # f = open(input_filename, 'r', encoding="cp949")
+    f = codecs.open(input_filename, "r", encoding)
+    for line in f.read().splitlines() :
         line = clean_text(line)
-        if len(line)==0 or line==None:
+        if line==None or len(line)==0 :
             continue
-        #print(line)
         data.append(line)
 
     result = attach_sameturn(data)
@@ -139,16 +148,68 @@ def subtitle_converter(input_filename, output_filename):
     with open(output_filename, 'w', encoding='cp949', newline='') as f:
         writer = csv.writer(f)
         for oq, oa in zip(query, answer):
-            cont = [oq, oa]
-            writer.writerow(cont)
+            conv = [oq, oa]
+            writer.writerow(conv)
 
 
-def createDictFromSentences(datas):
-    temp=[]
-    for sent in datas:
-        temp.extend(sent)
-    dict = {c:i for i, c in enumerate(temp)}
+def remove_empty_line(sentences):
+    rmidx = 0
+    while rmidx < len(sentences):
+        if len(sentences[rmidx])==0:
+            sentences.remove(sentences[rmidx])
+        else :
+            rmidx+=1
+    return sentences
+
+#수작업 후처리
+def remove_empty_from_csv(filename, encoding='cp949'):
+    original_query=[]
+    original_answer=[]
+    with open(filename, 'r', encoding=encoding) as f:
+        datareader = csv.reader(f, delimiter=',')
+        for row in datareader:
+            original_query.append(row[0])
+            original_answer.append(row[1])
+    print(original_query)
+    print(original_answer)
+    original_query = remove_empty_line(original_query)
+    original_answer = remove_empty_line(original_answer)
+    print(original_query)
+    print(original_answer)
+
+    with open(filename, 'w', encoding=encoding, newline='') as f:
+        writer = csv.writer(f)
+        for oq, oa in zip(original_query, original_answer):
+            conv = [oq, oa]
+            writer.writerow(conv)
+
+
+def createDictFromSentences(sentences):
+    '''    
+    :param sentences: the datas to be embedded
+    :return: zero padded dict
+    '''
+    temp=[] # 중복을 나중에 없앨 딕셔너리 단어들.
+    temp.append('<PAD>')
+    temp.append("<START>")
+    temp.append("<EOS>")
+    temp.append("<UNK>")
+    for sent in sentences:
+        temp.extend(sent) # be flatted
+
+    #딕셔너리 생성 단계
+    dict_idx = [i for i in range(len(temp))]
+    dict_len = len(dict_idx)
+
+    dict={}
+    unit_mat = np.eye(dict_len)
+    for token, idx in zip(temp, dict_idx):
+        dict[token] = unit_mat[idx].tolist()
+
     return dict
+
+
+
 
 #그리고 disintegrator를 따로 만들어야할 듯. 거기서 다시 로딩
 #이후 FQ, FA를 만들어야한다. (서버에서 유지할 리소스로서 전처리된 리소스를 가지고 있어야 할 듯 하다, 즉 load_data는 리소스를 가져오고, 임베딩하고, train, test 데이터로 구분해서 주는 작업 정도만 포함해야할 것이다.)
@@ -158,12 +219,12 @@ def disintegrator(sentences):
     :return: FS, OS In order to implement Grammar Generator
     '''
     original_sentences = [] # GG를 위한 톤 보존
-    disintegrated_sentences = [] # BE를 위한 톤, 조사 삭제
+    disintegrated_sentences = [] # BE를 위한 톤 삭제
     for sentence in sentences:
         original_sentences.append(['/'.join(tags) for tags in konlpy.tag.Twitter().pos(sentence)])
         disintegrated_sentences.append(['/'.join(tags) for tags in konlpy.tag.Twitter().pos(sentence, norm=True, stem=True)])
 
-    inputData=[]
+    inputData=[] # BE를 위한 톤, 조사 등 삭제
     for sent in disintegrated_sentences:
         result = []
         for token in sent:
@@ -171,7 +232,7 @@ def disintegrator(sentences):
                 result.append(token)
         inputData.append(result)
 
-    outputData = []
+    outputData = [] # GG를 위한 톤 보존
     for sent in original_sentences:
         result=[]
         for token in sent:
@@ -187,8 +248,12 @@ def get_emotion_score(sentence):
 # FQ, FA 잘린거로부터 가져와서 train, test 나눠서 반환할 메소드(근데 이건 load_data()에서 받아서 해도 된다.
 # 한편 부경대 팀이 Sentence Generator를 만들려면 FA에 대한 OA도 필요하다.X FS에 대한 OS가 있으면 된다. For Grammar Generator
 # 가장 중요한 것은 전달할 파라미터를 맞추는 것이다, Set이다. (V차원의 단어벡터(FQ), 각 단어의 감정 스코어, 각 단어의 4차원 스코어, FA, FS, OS) 4개는 Bot Engine, 2개는 Grammar Generator
-def load_n_embedding(filename, save=0):
-
+def load_handwork_n_create_dict(filename, dictfile_forBE, dictfile_forGG, save=0):
+    '''    
+    :param filename: csv file on handwork
+    :param save: to train generative models it need to get a embedded data and it should have a integrities(validation, keeping consistancy) for these save dict, to contain these works save parameter is designed       
+    :return tokens of sentences for BE, GG to be mapping into |V|
+    '''
     # 수작업 전처리된 CSV 로딩
     original_query = []
     original_answer=[]
@@ -202,54 +267,99 @@ def load_n_embedding(filename, save=0):
     (fq_input, oq_output) = disintegrator(original_query)
     (fa_input, oa_output) = disintegrator(original_answer)
 
-    # Grammar Generator를 위한 데이터
-    inputdata_forGG = fq_input + fa_input
-    outputdata_forGG = oq_output + oq_output
+    # Grammar Generator를 위한 input, output 데이터
+    inputdata_forGG = fq_input + fa_input#list of sentences, each sentence is tokenized
+    outputdata_forGG = oq_output + oa_output
 
     if save==1:
         #각 단어를 임베딩해서 저장해야함.
         Dict_forBE = createDictFromSentences(inputdata_forGG)
-        with open("dict_for_be.txt", 'w') as f:
+        with open(dictfile_forBE, 'w') as f:
             json.dump(Dict_forBE, f)
 
         Dict_forGG = createDictFromSentences(inputdata_forGG+outputdata_forGG)
-        with open("dict_for_gg.txt", 'w') as f:
+        with open(dictfile_forGG, 'w') as f:
             json.dump(Dict_forGG, f)
 
     return (fq_input, fa_input, inputdata_forGG, outputdata_forGG)
 
-def beEmbedded(sentences, dict):
+def beEmbedded(sentences, dict, unk_symbol="<UNK>"):
+    '''    
+    :param sentences: sentences to be replaced into number 
+    :param dict: dict to be referred
+    :return: embedded sentences 
+    '''
     result=[]
     for sent in sentences:
         embeddedsent = []
         for token in sent:
-            embeddedsent.append(dict[token])
+            try:
+                embeddedsent.append(dict[token])
+            except KeyError as ke:
+                embeddedsent.append(dict[unk_symbol])
         result.append(embeddedsent)
     return result
 
-# 최종적으로 load_data는 적절한 질의응답이 결정되고, 이들로부터 임베딩된 단어들을 반환해줘야한다. 그리고 감정 스코어도 저장된 파일로부터 산출해서 반환한다.
+def bePadding(sentences, max_pad_len, pad_symbol='<PAD>'):
+    for sent in sentences:
+        sen_len = len(sent)
+        for i in range(max_pad_len-sen_len):
+            sent.append(pad_symbol)
+    return sentences
+
+def beTagging(sentences, start_symbol='<START>', end_symbol='<EOS>'):
+    '''    
+    :param sentences: treated sentences
+    :param start_symbol: symbol to be adapted
+    :param end_symbol: symbol to be adapted
+    :return: tagged input for decoder, tagged output for target 
+    '''
+    dec_input = []
+    dec_output = []
+    for sent in sentences:
+        temp = sent.copy()
+        temp.insert(0, start_symbol)
+        dec_input.append(temp)
+        sent.append(end_symbol)
+        dec_output.append(sent)
+    return (dec_input, dec_output)
+
+# 최종적으로 load_data는 적절한 질의응답(최종 입출력)이 결정되고, 이들로부터 임베딩된 단어들을 반환해줘야한다. 그리고 감정 스코어도 저장된 파일로부터 산출해서 반환한다.
 def load_data(QAfile, BEdictfile, GGdictfile):
-    FQ, FA, inputdata_forGG, outputdata_forGG = load_n_embedding(QAfile)
+    FQ, FA, inputdata_forGG, outputdata_forGG = load_handwork_n_create_dict(QAfile, BEdictfile, GGdictfile)
+
+    FQ_enc_input = bePadding(FQ, 40)
+    (FA_dec_input, FA_dec_output) = beTagging(FA)
+
+    enc_input_forGG = bePadding(inputdata_forGG, 40)
+    (outputdata_forGG_dec_input, outputdata_forGG_dec_output) = beTagging(outputdata_forGG)
 
     with open(BEdictfile) as f:
         BEDict = json.load(f)
-        embedded_FQ = beEmbedded(FQ, BEDict)
-        embedded_FA = beEmbedded(FA, BEDict)
+        embedded_FQ_enc_input = beEmbedded(FQ_enc_input, BEDict)
+        embedded_FA_dec_input = beEmbedded(FA_dec_input, BEDict)
+        embedded_FA_dec_output = beEmbedded(FA_dec_output, BEDict)
+
 
     with open(GGdictfile) as f:
         GGDict = json.load(f)
-        embedded_input_forGG = beEmbedded(inputdata_forGG, GGDict)
-        embedded_output_forGG = beEmbedded(outputdata_forGG, GGDict)
+
+        embedded_enc_input_forGG = beEmbedded(enc_input_forGG, GGDict)
+        embedded_dec_output_forGG = beEmbedded(outputdata_forGG_dec_input, GGDict)
+        embedded_dec_output_forGG = beEmbedded(outputdata_forGG_dec_output, GGDict)
 
     # 각 단어에 대한 감정 산출 ** 이 부분이 이야기 되어야한다. 각 단어별 감정 스코어 갈지, 문장의 감정 스코어 갈지. 그리고 4차원 감정 스코어는 각 단어에 대해서 넣을지.(대표값 선택은 최대값을 선택해야할 것 임.)
     # 감정 스코어는 FQ 문장에 대한 감정 스코어를 가져와야한다.
     # get_emotion_score(sentence)가 문장에 대한 스코어를 주면 될 듯 하다.
 
-    return (embedded_FQ, embedded_FA, embedded_input_forGG, embedded_output_forGG)
+    return (embedded_FQ_enc_input, embedded_FA_dec_input,embedded_FA_dec_output, embedded_enc_input_forGG, embedded_dec_output_forGG,embedded_dec_output_forGG)
 
-#subtitle_converter("1.txt", "data.csv")
-#load_n_embedding("data.csv", save=1)
-#(embedded_FQ, embedded_FA, embedded_input_forGG, embedded_output_forGG) = load_data("data.csv", "dict_for_be.txt","dict_for_gg.txt")
+#subtitle_converter("data_staryou.txt", "data_staryou.csv")
+#remove_empty_from_csv("data_staryou.csv")
+#load_handwork_n_create_dict("data_staryou.csv", "dict_for_be.txt","dict_for_gg.txt",save=1)
+
+#(embedded_FQ, embedded_FA, embedded_input_forGG, embedded_output_forGG) = load_data("data_staryou.csv", "dict_for_be.txt","dict_for_gg.txt")
+load_data("data_staryou.csv", "dict_for_be.txt","dict_for_gg.txt")
 #print(len(embedded_FQ))
 #print(embedded_FQ)
 
@@ -260,12 +370,15 @@ Numpy, koNLPy
 1. *.smi 파일을 모조리 가져와서 하나의 txt파일로 만들어라.
 2. subtitle_converter로 그걸 가져와서 어느정도는 프로그램으로 전처리한다. 이후 csv로 저장(타임라인 제거, 태그제거, 영어 노래 제거, ?가 연속일때 포함해서 쿼리로 처리, 그 다음 문장을 응답으로 처리, 특수한 고유명사에 대해서 대체어로 대체, 너무 긴 질의응답 제거)     
 3. 수작업으로 나머지 전처리한다.(어색한 질의응답 제거, 파악하지 못한 특수 인명 사용 여부)
+3-1. remove_empty_from_csv()으로 후처리 한다.
 4. load_n_embedding으로 위 3번 작업후 csv파일 로딩하고 Bot Engine과 Grammar Engine에 필요한 데이터로 각각 나누고 적당히 자른다음 태그를 붙여서 원핫으로 임베딩한다. 그리고 딕셔너리를 저장한다.
+load_n_embedding(,save=1)
 5. load_data로 딕셔너리만 가져온다. 딕셔너리는 Bot Engine과 Grammar Engine을 위한 각각의 train, test용 데이터를 적용직전 상태로 저장되어있다.
 
 Bot Engine 개발 팀은 해당 파일을 임포트하고 딕셔너리 파일 받아서 load_data()만 호출하면된다.
 그러기 위해서 패딩이 들어가야한다. 이를 위해서는 봇 엔진팀과의 상의가 필요하다. 패딩은 뭘로 넣어야하는가. seq2seq 명세를 다루며 이야기가 되어야할 것이다. 
 
+그리고, GG를 위한 데이터는 BE의 2배가 아니고 아주 엄청나게 많이 쓸수도 있겠어. 널린게 완성된 문장들이니까.
 다만 Bot Engine에서 seq2seq 구현시 사용할 플래그(e.g <GO>,<EOS> etc)가 필요하다면 필요에따라 딕셔너리에 추가해야한다. 
 
 '''
